4449ec2d47fa894508d30fa9a908616168e49e89
wmt-krunald <krunald@webmob.tech>
2022-02-07 17:33:33 +0530

    Working with numbers

---
+++ Working_Numbers/work_number.py
@@ -0,0 +1,28 @@
+print (5) #print the number
+
+print (3 + 4) #perform arithmetic operation like: + - * /
+print (5 % 2) #give reminder
+
+print (10 * ( 2 + 3 )) #first 2+3 and multiply with 10
+
+n1 = 10
+print (n1) #print the number
+print (str(n1)) #convert into string
+print (str(n1) + " is my Roll Number.") #add another string
+
+n2 = -5
+print (abs(n2)) #give absoulate value. In this case 5
+
+print (pow(10, 2)) #give 10^2 = 100
+print (min(50, 12)) #give minimum number
+print (max(50, 12)) #give maximum number
+
+print (round(2.5)) #give roundoff number
+
+
+#We import MATH function to do some another task
+from math import *
+
+print (floor(3.6)) #give floor of 3.6
+print (ceil(3.6))  #give ceil of 3.6
+print (sqrt(25)) #give sqrt of 25
