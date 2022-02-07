c7d4ece581b3daae6823f471c419605a75e9efd1
wmt-krunald <krunald@webmob.tech>
2022-02-07 15:48:27 +0530

    Variable and Data types program

---
+++ Variables_datatypes/simple.py
@@ -0,0 +1,9 @@
+dept_name = "DevOps"
+dept_member = "Project Lead"
+
+print ("This is " + dept_name + " Environment.")
+print ("They are work with " + dept_member + ".")
+
+dept_member_name = "Krunal"
+print ("One of the member of " + dept_name + " is " + dept_member_name + ".")
+print ("He is Trainess.")
