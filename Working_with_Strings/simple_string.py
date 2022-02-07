f4acb96a1d0cd06c80e94692600afbd4075097ca
wmt-krunald <krunald@webmob.tech>
2022-02-07 16:56:42 +0530

    Simple String Program

---
+++ Working_with_Strings/simple_string.py
@@ -0,0 +1,9 @@
+#This is Working with string Program
+print ("WebMob Technologies") #print in single line
+
+print ("WebMob\nTechnologies") #Print in new line
+print ("Webmob\"Technologies") #print " character
+print ("WebMob\Technologies") #print \ character
+
+msg = "WebMob Technologies"
+print (msg) #print variable content
