11727d1a4e07070e78eb56f45767798effcbe7e4
wmt-krunald <krunald@webmob.tech>
2022-02-07 18:31:25 +0530

    Mad Lib Game

---
+++ Mad_Lib_Game/simple.py
@@ -0,0 +1,9 @@
+name = input("Enter Your Name : ")
+cartoon = input("Enter The Cartoon Name which You liked :")
+charac = input("Your Favourite Character : ")
+
+print("Heyy " + name + "!")
+print( name + " is like the Cartoon shows.")
+print("and " + name + "'s Favourite Cartoon is " + cartoon)
+print("and in this " + cartoon + ", " + name + "'s favourite character is " + charac)
+print("Are You know one funny thing, " + name + " is look like " + charac + " :):):)")
