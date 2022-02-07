d02f6c069e8ea908fbe3a35c6f7053eb888aacf1
wmt-krunald <krunald@webmob.tech>
2022-02-07 18:00:31 +0530

    Basic Add Calculator

---
+++ Basic_cal/basic_calculator.py
@@ -0,0 +1,8 @@
+n1 = input("Enter the First Number: ")
+n2 = input("Enter the second Number: ")
+
+#When we get input from the user By Default python convert into string
+#For that we are using float function
+result = float(n1) + float(n2)
+
+print(result)
