59b2befa13b022fe04d68f2de2a257c53dd6e89d
wmt-krunald <krunald@webmob.tech>
2022-02-07 17:47:21 +0530

    Get Input from User

---
+++ Input_User/simple.py
@@ -0,0 +1,5 @@
+name = input("Enter Your Company Name : ")
+stipend = input("Enter Your Stipend : ")
+
+print ( name + " is awesome for your currier.")
+print ( name + " is provide " + stipend + " stipend only.")
