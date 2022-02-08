fe4aeeeb75828adb639d175f10f83cf1877612a9
wmt-krunald <krunald@webmob.tech>
2022-02-08 12:02:56 +0530

    Basic tuples example

---
+++ Tuples/simple.py
@@ -0,0 +1,15 @@
+from re import X
+
+
+number = (10, 20, 30)
+
+print(number) #print the tuple
+print(number[1]) #print index 1 element
+
+#We can have group of tuples
+x = [(1, 2), (3, 4), (5, 6)]
+print(x[1])
+
+#THE MAIN DIFFERENCE BETWEEN TUPLES AND LIST
+#TUPLES CANNOT BE CHANGED OR MODIFY AFTER CREATING
+#LIST CAN BE MODIFYABLE AND CHANGEABLE
