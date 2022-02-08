4ecfacf3a93c6fa20247a7abdc0c5a5c6206ac7c
wmt-krunald <krunald@webmob.tech>
2022-02-08 12:55:11 +0530

    Return Function

---
+++ Function/return_func.py
@@ -0,0 +1,9 @@
+#This is return Function
+def power(n, m):
+    return pow(n, m) #this will return the value
+
+print(power(5, 2)) #print the value
+
+#we can also use
+#result = power(5, 2)
+#print(result)
