598948f55ff803717e89eb1a07365df467e7e354
wmt-krunald <krunald@webmob.tech>
2022-02-08 12:56:47 +0530

    Return Function

--- Function/return_func.py
+++ Function/return_func.py
@@ -1,6 +1,8 @@
 #This is return Function
 def power(n, m):
     return pow(n, m) #this will return the value
+# return function break the function and comeout from function
+# return fuction is return int , string value.
 
 print(power(5, 2)) #print the value
 
