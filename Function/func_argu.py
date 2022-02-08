68b632e1eec814417fe8446fa46ddbe8ab4e1b91
wmt-krunald <krunald@webmob.tech>
2022-02-08 12:40:23 +0530

    Operation of Function

---
+++ Function/func_argu.py
@@ -0,0 +1,12 @@
+#This name function take input from user and greeting.
+def wel_user():
+    na1 = input("Enter Your Name : ")
+    print("Welcome " + na1 + ", in world of Python\n")
+
+wel_user()
+
+def uname(name, age):
+    print("Welcome " + name + ", Your age is " + str(age) + "." )
+
+uname("Krunal", 21) # In above we write str(age) instead of this we can write in calling function time
+uname("Mayur", 25)    #... uname("Krunal", "21"). this 21 is consider as a string.
