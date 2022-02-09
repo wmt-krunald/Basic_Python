5a771cf525f656c1ba249e5402721d5f3654f467
wmt-krunald <krunald@webmob.tech>
2022-02-09 16:27:34 +0530

    Representation of Try/Except

---
+++ Try-Except/try_except.py
@@ -0,0 +1,9 @@
+#This is Try and Except Function.
+
+#Any kind of error will display as Invalid input.
+
+try:
+    n1 = int(input("\nEnter Any Number : "))
+    print(n1)
+except:
+    print("\nInvalid Input")
