224dbb664fce1f2cbd0051f4549d705d521dcc09
wmt-krunald <krunald@webmob.tech>
2022-02-09 10:25:54 +0530

    Better Calculator Perform Arithmetic Operation

---
+++ Calculater/opera_calcu.py
@@ -0,0 +1,16 @@
+n1 = float(input("Enter First Number = "))
+op = input("\nEnter Operator : ")
+n2 =float(input("\nEnter Second Number = "))
+
+if op == "+":
+    print("\nYour Answer is : ", n1 + n2)
+elif op == "-":
+    print("\nYour Answer is : ", n1 - n2)
+elif op == "*":
+    print("\nYour Answer is : ", n1 * n2)
+elif op == "/":
+    print("\nYour Answer is : ", n1 / n2)
+elif op == "%":
+    print("\nYour Answer is : ", n1 % n2)
+else:
+    print("Enter Valid Operator....!")
