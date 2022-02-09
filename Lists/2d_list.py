16a808a0ef82ba183fcd0f45d83755b5a3a5c84e
wmt-krunald <krunald@webmob.tech>
2022-02-09 15:13:12 +0530

    2D List and represent

--- Lists/2d_list.py
+++ Lists/2d_list.py
@@ -3,9 +3,10 @@ number_list = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
-    [0, "s", 1]
+    [0, "s", True]
 ]
 
 print(number_list)  #It will print list in row wise
 print(number_list[0][0])    #It will print 0th row and 0th col value
 print(number_list[3][1])    #It will print 2D list's 3rd row and 2st col value
+print(number_list[3][2])    #It will print boolean value which is located at 3rd row and 2nd col
