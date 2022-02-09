232eb91d0d03744f8f1605a515620dd81b746747
wmt-krunald <krunald@webmob.tech>
2022-02-09 15:21:02 +0530

    Nested For Loop Example

---
+++ Loop/For_Loop/nested_forloop.py
@@ -0,0 +1,10 @@
+number_list = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0, "s", True]
+]
+
+for row in number_list:
+    for col in row:
+        print(col)      #This will print all the element of the 2D list with the help of Nested ForLoop
